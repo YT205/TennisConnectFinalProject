b7ce4d244d4d222cafa551ae22686650b4b678e2
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2022-02-09 23:56:55 -0600

    markers working

--- navigation/Maps.js
+++ navigation/Maps.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 // import MapPage from "./MapPage";
 // import { SafeAreaView } from "react-navigation";
@@ -17,14 +17,77 @@ const styles = StyleSheet.create({
 
 export default function Maps() {
   return (
-    <View style={styles.container}>
       <MapView
         style={{height: '100%', width: '100%'}}
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
-      />
-    </View>
-    
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.035
+        }}>
+        <Marker
+          coordinate = {{
+          latitude: 42.0166211,
+          longitude: -88.0741795,
+          }}
+          description = {'2 Courts, Lights Available'}
+          title = {'Meineke Recreation Center'}>
+          
+        </Marker>
+
+        <Marker
+          coordinate = {{
+          latitude: 42.0035238,
+          longitude: -88.0084918,
+          }}
+          description = {'2 Courts, Lights Available'}
+          title = {'Clark Park'}>
+          
+        </Marker>
+
+        <Marker
+          coordinate = {{
+          latitude: 42.033988,
+          longitude: -88.050479,
+          }}
+          description = {'2 Courts, No Lights Available'}
+          title = {'Park St Claire'}>
+          
+        </Marker>
+
+        <Marker
+          coordinate = {{
+          latitude: 42.0312903,
+          longitude: -88.119026,
+          }}
+          description = {'2 Courts, No Lights Available'}
+          title = {'Hoover Park'}>
+          
+        </Marker>
+
+        <Marker
+          coordinate = {{
+          latitude: 42.0061832,
+          longitude: -88.0572353,
+          }}
+          description = {'2 Courts, No Lights Available'}
+          title = {'Kingsport East Courts'}>
+          
+        </Marker>
+
+        <Marker
+          coordinate = {{
+          latitude: 42.0010683,
+          longitude: -88.0689048,
+          }}
+          description = {'1 Court, No Lights Available'}
+          title = {'Sunset Park'}>
+          
+        </Marker>
+
+      </MapView>
   );
 }
 
